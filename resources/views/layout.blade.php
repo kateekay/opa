66c7ad95254c98740193f04e1324c31a2f6d2482
kate <kkbug1216@gmail.com>
2017-08-27 20:09:03 -0500

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+    <title>Omega Phi Alpha - Omicron</title>
+</head>
+<body>
+
+@yield('content')
+
+
+
+</body>
+</html>
